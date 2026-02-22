62e00e7a5cfeaa42782e9bde95c96aff210bfcde
MO7AMED3TWAN <MO7AMED3TWAN@gmail.com>
2026-02-22 05:21:36 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/3twan/', // Your repository name
+  base: '/', // Your repository name
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
